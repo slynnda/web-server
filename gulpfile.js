188fa7c9f0f2c1e1a7f30065ad6d1fbdee2c14b1
sophia.hudson <sophia.hudson@technicallyspeaking.io>
2016-12-30 08:26:58 -0700

    Adding gitignore, and getting rudimentary TypeScript build working

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+const gulp = require('gulp'),
+      ts = require('gulp-typescript'),
+      tsProject = ts.createProject("tsconfig.json")
+
+gulp.task("default", () => {
+  return tsProject.src()
+    .pipe(tsProject())
+    .js.pipe(gulp.dest("dist"))
+})
